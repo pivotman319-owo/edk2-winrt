63c23b4ad57dd64aafc115fc3ac61bf211bbe4d1
gailium119 <77530553+gailium119@users.noreply.github.com>
2023-05-25 16:03:51 +0800

    Update aptchange.sh

--- aptchange.sh
+++ aptchange.sh
@@ -3,7 +3,7 @@
 
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse > /etc/apt/sources.list
-echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse >> /etc/apt/sources.list
-echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse >> /etc/apt/sources.list
-echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse >> /etc/apt/sources.list
+echo deb http://archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse > /etc/apt/sources.list
+echo deb http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse >> /etc/apt/sources.list
+echo deb http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse >> /etc/apt/sources.list
+echo deb http://archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse >> /etc/apt/sources.list
